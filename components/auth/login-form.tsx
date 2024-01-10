0e2c38f765d557977df4d76f89820662fd119fce
mationate <mationate.0@gmail.com>
2024-01-10 15:39:05 -0300

    first commitç

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,14 @@
+import { CardWrapper } from "./card-wrapper"
+
+export const LoginForm = () => {
+    return(
+       <CardWrapper 
+       headerLabel="Bienvenid@ !"
+        backButtonLabel="No tienes cuenta? Registrate"
+        backButtonHref="/auth/register"
+        showSocial
+       >
+                asd
+        </CardWrapper>
+    )
+}
